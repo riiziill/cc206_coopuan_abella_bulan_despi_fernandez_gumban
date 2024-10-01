2c753d2b7be1ab25c54a0e028ed16c0cd6e79c24
rj-115 <rhestyjohn.fernandez@wvsu.edu.ph>
2024-10-01 23:15:53 +0800

    fix

--- lib/features/sign_in.dart
+++ lib/features/sign_in.dart
@@ -8,54 +8,48 @@ class signIn extends StatelessWidget {
         title: Text('Sign Up'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'USERNAME',
-                  border: OutlineInputBorder(),
-                ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
+          children: [
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'USERNAME',
+                border: OutlineInputBorder(),
               ),
-              SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'WVSU EMAIL',
-                  hintText: 'Example: abc@wvsu.edu.ph',
-                  border: OutlineInputBorder(),
-                ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'WVSU EMAIL',
+                hintText: 'Example: abc@wvsu.edu.ph',
+                border: OutlineInputBorder(),
               ),
-              SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'WVSU ID',
-                  hintText: 'Example: 2022M0000',
-                  border: OutlineInputBorder(),
-                ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'WVSU ID',
+                hintText: 'Example: 2022M0000',
+                border: OutlineInputBorder(),
               ),
-              SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'WVSU PASSWORD',
-                  border: OutlineInputBorder(),
-                ),
-                obscureText: true,
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'WVSU PASSWORD',
+                border: OutlineInputBorder(),
               ),
-              SizedBox(height: 20),
-              Spacer(),
-              Container(
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Sign Up'),
-                ),
-              ),
-              SizedBox(height: 200),
-            ],
-          ),
+              obscureText: true,
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Sign Up'),
+            ),
+            SizedBox(height: 10),
+          ],
         ),
       ),
     );
