0190fd0c889d0d80156d9fd54c47aeaff6d1d085
rj-115 <rhestyjohn.fernandez@wvsu.edu.ph>
2024-10-02 10:52:45 +0800

    changes in design

--- lib/features/sign_in.dart
+++ lib/features/sign_in.dart
@@ -5,51 +5,82 @@ class signIn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sign Up'),
-        centerTitle: true,
+        title: Text('Back to Login',  
+              style: TextStyle(
+              fontSize: 15,
+              color: const Color.fromARGB(255, 10, 1, 112),
+              fontWeight: FontWeight.bold),),
+        
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ListView(
-          children: [
-            SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'USERNAME',
-                border: OutlineInputBorder(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(height: 10),
+              SizedBox(
+                width: 300,
+                child: Text('SIGN UP',
+                 style: TextStyle(
+                 fontSize:30,
+                 color: const Color.fromARGB(255, 10, 1, 112),
+                 fontWeight: FontWeight.bold,
+                 )
+                ),
               ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'WVSU EMAIL',
-                hintText: 'Example: abc@wvsu.edu.ph',
-                border: OutlineInputBorder(),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'USERNAME',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'WVSU ID',
-                hintText: 'Example: 2022M0000',
-                border: OutlineInputBorder(),
+              SizedBox(height: 10),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'WVSU EMAIL',
+                    hintText: 'Example: abc@wvsu.edu.ph',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'WVSU PASSWORD',
-                border: OutlineInputBorder(),
+              SizedBox(height: 10),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'WVSU ID',
+                    hintText: 'Example: 2022M0000',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
               ),
-              obscureText: true,
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Sign Up'),
-            ),
-            SizedBox(height: 10),
-          ],
+              SizedBox(height: 10),
+              SizedBox(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'WVSU PASSWORD',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 100, 
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Sign Up'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
