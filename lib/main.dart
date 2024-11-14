bc52cb09e097a1d8f3f7bf50dcf407bddfd62fea
Kent-J-A <kentjasper.abella@wvsu.edu.ph>
2024-11-14 22:50:48 +0800

    Added reservation page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:cc206_magic_calculator_abella_bulan_despi_fernandez_gumban/features/reservation.dart';
 import 'package:cc206_magic_calculator_abella_bulan_despi_fernandez_gumban/features/sign_in.dart';
 import 'package:cc206_magic_calculator_abella_bulan_despi_fernandez_gumban/features/log_in.dart';
+import 'package:cc206_magic_calculator_abella_bulan_despi_fernandez_gumban/features/reservation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -34,12 +36,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute: 'logIn',
-      routes: {
-        'logIn': (BuildContext ctc) => logIn(),
-        'signIn': (BuildContext ctc) => signIn(),
-      }
-//       home: signIn(),
+      // initialRoute: 'logIn',
+      // routes: {
+      //   'logIn': (BuildContext ctc) => logIn(),
+      //   'signIn': (BuildContext ctc) => signIn(),
+      // }
+      home: Reservation(),
     );
   }
 }
